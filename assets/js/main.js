07e025a444875aab1a7877e1617bf228b47044e2
llasen <llasen@llasen.168.15.17>
2021-06-10 01:42:11 -0300

    add my projects

--- assets/js/main.js
+++ assets/js/main.js
@@ -83,7 +83,19 @@ skillsHeader.forEach((el) => {
 
 
 /*===== SWIPER CAROUSEL =====*/ 
+let swiper = new Swiper('.portfolio__container', {
+    cssMode: true,
+    loop: true,
 
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+      pagination: {
+      el: '.swiper-pagination',
+      clickable: true
+    },
+    });
 
 /*===== GSAP ANIMATION =====*/ 
 
